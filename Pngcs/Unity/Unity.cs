7e44431bf188c1e3f4ed5cbdd620bd72e630adbe
Andrzej <andrew.r.lukasik@gmail.com>
2019-02-06 23:08:20 +0100

    Unity-specific api: PNG.WRITE and PNG.READ (using Pngcs.Unity namespace)

---
+++ Pngcs/Unity/Unity.cs
@@ -0,0 +1,253 @@
+﻿using System.Threading.Tasks;
+using IO = System.IO;
+
+using UnityEngine;
+
+namespace Pngcs.Unity
+{
+    public static class PNG
+    {
+
+        /// <summary> Write PNG to file </summary>
+        public static void WRITE
+        (
+            Texture2D texture ,
+            string filePath
+        )
+        {
+            Debug.Log( $"WRITE: { texture.GetHashCode() } { texture.format } { filePath }" );
+            var pixels = texture.GetPixels();
+            var format = texture.format;
+            int bitDepth = GetBitDepth( format );
+            bool alpha = GetIsAlpha( format );
+            bool greyscale = GetIsGreyscale( format );
+            var info = new ImageInfo(
+                texture.width ,
+                texture.height ,
+                bitDepth ,
+                alpha ,
+                greyscale ,
+                false//not implemented here yet
+            );
+
+            // open image for writing:
+            PngWriter writer = FileHelper.CreatePngWriter( filePath , info , true );
+            
+            // add some optional metadata (chunks)
+            var meta = writer.GetMetadata();
+            meta.SetTimeNow( 0 );// 0 seconds fron now = now
+            meta.SetText( Chunks.PngChunkTextVar.KEY_Title , texture.name );
+            meta.SetText( Chunks.PngChunkTextVar.KEY_Software , $"UnityEngine { Application.unityVersion }" );
+            
+            int numRows = info.Rows;
+            int numCols = info.Cols;
+            for( int row=0 ; row<numRows ; row++ )
+            {
+                ImageLine imageline = new ImageLine( info );
+                int maxSampleVal = imageline.maxSampleVal;
+
+                //fill line:
+                if( greyscale==false )
+                {
+                    if( alpha )
+                    {
+                        for( int col=0 ; col<numCols ; col++ )
+                        {
+                            RGBA rgba = ToRGBA( pixels[ ToTexture2DIndex( row , col , numRows , numCols ) ] , bitDepth );
+                            ImageLineHelper.SetPixel( imageline , col , rgba.r , rgba.g , rgba.b , rgba.a );
+                        }
+                    }
+                    else
+                    {
+                        for( int col=0 ; col<numCols ; col++ )
+                        {
+                            RGB rgb = ToRGB( pixels[ ToTexture2DIndex( row , col , numRows , numCols ) ] , bitDepth );
+                            ImageLineHelper.SetPixel( imageline , col , rgb.r , rgb.g , rgb.b );
+                        }
+                    }
+                }
+                else
+                {
+                    if( alpha==false )
+                    {
+                        for( int col=0 ; col<numCols ; col++ )
+                        {
+                            int r = ToInt( pixels[ ToTexture2DIndex( row , col , numRows , numCols ) ].r , bitDepth );
+                            ImageLineHelper.SetPixel( imageline , col , r );
+                        }
+                    }
+                    else
+                    {
+                        for( int col=0 ; col<numCols ; col++ )
+                        {
+                            int a = ToInt( pixels[ ToTexture2DIndex( row , col , numRows , numCols ) ].a , bitDepth );
+                            ImageLineHelper.SetPixel( imageline , col , a );
+                        }
+                    }
+                }
+                
+                //write line:
+                writer.WriteRow( imageline , row );
+            }
+            writer.End();
+        }
+
+        /// <summary> Create Texture2D from PNG file </summary>
+        public static async Task<Texture2D> READ ( string filePath )
+        {
+            PngReader reader = FileHelper.CreatePngReader( filePath );
+            var info = reader.ImgInfo;
+            int numRows = info.Rows;
+            int numCols = info.Cols;
+            int channels = info.Channels;
+            int bitDepth = info.BitDepth;//bits per sample (channel) in the buffer
+            int bytesPixel = info.BytesPixel;
+            if( info.Indexed ) { throw new System.NotImplementedException( "indexed png not implemented" ); }
+
+            //select appropriate texture format:
+            TextureFormat textureFormat;
+            if( bitDepth==16 && channels==1 )
+            {
+                textureFormat = TextureFormat.R16;
+            }
+            else { throw new System.NotImplementedException( $"bit depth '{ bitDepth }' for '{ channels }' channels not implemented" ); }
+            
+            //create pixel array:
+            Color[] pixels = new Color[ numCols * numRows ];
+            await Task.Run( ()=> { try {
+
+                for( int row=0 ; row<numRows ; row++ )
+                {
+                    ImageLine imageLine = reader.ReadRowInt( row );
+                    var scanline = imageLine.Scanline;
+                    if( imageLine.SampleType==ImageLine.ESampleType.INT )
+                    {
+                        for( int col=0 ; col<numCols ; col++ )
+                        {
+                            var color = new Color();
+                            for( int ch=0 ; ch<channels ; ch++ )
+                            {
+                                int raw = scanline[ col * channels + ch ];
+                                float rawMax = GetBitDepthMaxValue( bitDepth );
+                                float value = (float)raw / rawMax;
+
+                                //
+                                if( ch==0 ) { color.r = value; }
+                                else if( ch==1 ) { color.g = value; }
+                                else if( ch==2 ) { color.b = value; }
+                                else if( ch==3 ) { color.a = value; }
+                                else { throw new System.Exception( $"channel { ch } not implemented" ); }
+                            }
+                            pixels[ ToTexture2DIndex( row , col , numRows , numCols ) ] = color;
+                        }
+                    }
+                    else { throw new System.Exception( $"imageLine.SampleType { imageLine.SampleType } not implemented" ); }
+                }
+
+            } catch ( System.Exception ex ) { Debug.LogException( ex ); } } );
+
+            //create texture
+            Texture2D result = new Texture2D( numCols , numRows , textureFormat , false , true );
+            result.wrapMode = TextureWrapMode.Clamp;
+            result.SetPixels( pixels );
+            result.Apply();
+
+            return result;
+        }
+
+        /// <summary> Texture2D's rows start from the bottom while PNG from the top. Hence inverted y/row.  </summary>
+        public static int ToTexture2DIndex ( int row , int col , int numRows , int numCols ) => numCols * ( numRows - 1 - row ) + col;
+        
+        public static int GetBitDepth ( TextureFormat format )
+        {
+            switch ( format )
+            {
+                case TextureFormat.Alpha8: return 8;
+                case TextureFormat.R8: return 8;
+                case TextureFormat.R16: return 16;
+                case TextureFormat.RHalf: return 16;
+                case TextureFormat.RFloat: return 32;
+                case TextureFormat.RGB24: return 8;
+                case TextureFormat.RGBA32: return 8;
+                case TextureFormat.ARGB32: return 8;
+                default: throw new System.NotImplementedException( $"format '{ format }' not implemented" );
+            }
+        }
+
+        public static bool GetIsAlpha ( TextureFormat format )
+        {
+            switch ( format )
+            {
+                case TextureFormat.Alpha8: return true;//? im not 100% sure is it alpha or, for example, red channel
+                case TextureFormat.R8: return false;
+                case TextureFormat.R16: return false;
+                case TextureFormat.RHalf: return false;
+                case TextureFormat.RFloat: return false;
+                case TextureFormat.RGB24: return false;
+                case TextureFormat.RGBA32: return true;
+                case TextureFormat.ARGB32: return true;
+                default: throw new System.NotImplementedException( $"format '{ format }' not implemented" );
+            }
+        }
+
+        public static bool GetIsGreyscale ( TextureFormat format )
+        {
+            switch ( format )
+            {
+                case TextureFormat.Alpha8: return true;
+                case TextureFormat.R8: return true;
+                case TextureFormat.R16: return true;
+                case TextureFormat.RHalf: return true;
+                case TextureFormat.RFloat: return true;
+                default: return false;
+            }
+        }
+
+        public static int ToInt ( float color , int bitDepth )
+        {
+            float max = GetBitDepthMaxValue( bitDepth );
+            return (int)( color * max );
+        }
+
+        public static RGB ToRGB ( Color color , int bitDepth )
+        {
+            float max = GetBitDepthMaxValue( bitDepth );
+            return new RGB {
+                r = (int)( color.r * max ) ,
+                g = (int)( color.g * max ) ,
+                b = (int)( color.b * max )
+            };
+        }
+        
+        public static RGBA ToRGBA ( Color color , int bitDepth )
+        {
+            float max = GetBitDepthMaxValue( bitDepth );
+            return new RGBA {
+                r = (int)( color.r * max ) ,
+                g = (int)( color.g * max ) ,
+                b = (int)( color.b * max ) ,
+                a = (int)( color.a * max )
+            };
+        }
+
+        public static uint GetBitDepthMaxValue ( int bitDepth )
+        {
+            switch ( bitDepth )
+            {
+                case 1: return 1;
+                case 2: return 3;
+                case 4: return 15;
+                case 8: return byte.MaxValue;
+                case 16: return ushort.MaxValue;
+                case 32: return uint.MaxValue;
+                default: throw new System.Exception( $"bitDepth '{ bitDepth }' not implemented" );
+            }
+        }
+
+        public struct RGB { public int r, g, b; }
+
+        public struct RGBA { public int r, g, b, a; }
+
+    }
+
+}
