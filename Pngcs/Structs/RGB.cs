432880685ea447ab8483c0fbdaded3e35a864fc3
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2019-09-09 22:11:43 +0200

    no message

--- Pngcs/Structs/RGB.cs
+++ Pngcs/Structs/RGB.cs
@@ -6,4 +6,8 @@ namespace Pngcs
 		public T R, G, B;
 		public RGBA<T> RGBA ( T A ) => new RGBA<T>{ R=R , G=G , B=B , A=A };
 	}
+	public static class RGB
+	{
+		public static UnityEngine.Color ToColor ( RGB<int> rgb , float denominator ) => new UnityEngine.Color{ r=(float)rgb.R/denominator , g=(float)rgb.G/denominator , b=(float)rgb.B/denominator };
+	}
 }
--- Pngcs/Structs/RGBA.cs
+++ Pngcs/Structs/RGBA.cs
@@ -6,4 +6,8 @@ namespace Pngcs
 		public T R, G, B, A;
 		public RGB<T> RGB => new RGB<T>{ R=R , G=G , B=B };
 	}
+	public static class RGBA
+	{
+		public static UnityEngine.Color ToColor ( RGBA<int> rgba , float denominator ) => new UnityEngine.Color{ r=(float)rgba.R/denominator , g=(float)rgba.G/denominator , b=(float)rgba.B/denominator , a=(float)rgba.A/denominator };
+	}
 }
